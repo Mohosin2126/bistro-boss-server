7f361623651841032d02ab471438cc8def3931cf
Mohosin212601~ <mohosin2126@gmail.com>
2023-11-20 04:42:18 +0800

    added delete

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app=express()
 const cors=require('cors')
 const port=process.env.PORT||5000
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 
 
@@ -60,6 +60,13 @@ app.get("/carts",async(req,res)=>{
   const email=req.query.email
   const query={email: email}
   const result=await cartCollection.find(query).toArray()
+  res.send(result)
+})
+
+app.delete("/carts/:id",async(req,res)=>{
+  const id=req.params.id
+  const query={_id: new ObjectId(id)}
+  const result=await cartCollection.deleteOne(query)
   res.send(result)
 })
 
