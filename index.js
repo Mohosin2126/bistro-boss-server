eb8506a47767759e29cab31770de973f415150a9
Mohosin212601~ <mohosin2126@gmail.com>
2023-11-22 00:19:30 +0800

    added payment method server

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors=require('cors')
 const jwt = require('jsonwebtoken');
 const port=process.env.PORT||5000
 require('dotenv').config()
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 
@@ -222,6 +223,29 @@ app.delete("/carts/:id",async(req,res)=>{
   const result=await cartCollection.deleteOne(query)
   res.send(result)
 })
+
+
+// payment intent 
+app.post('/create-payment-intent', async (req, res) => {
+  const { price } = req.body;
+  const amount = parseInt(price * 100);
+  console.log(amount, 'amount inside the intent')
+
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: amount,
+    currency: 'usd',
+    payment_method_types: ['card']
+  });
+
+  res.send({
+    clientSecret: paymentIntent.client_secret
+  })
+});
+
+
+
+
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
